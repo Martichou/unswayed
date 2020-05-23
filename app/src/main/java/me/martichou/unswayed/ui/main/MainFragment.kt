df666094d0b30705c866acf5c1935d1fb0d7ac83
Martichou <martichou.andre@gmail.com>
2020-05-23 20:06:12 +0200

    Setup the base of the incoming project, main dependencies added

--- app/src/main/java/me/martichou/unswayed/ui/main/MainFragment.kt
+++ app/src/main/java/me/martichou/unswayed/ui/main/MainFragment.kt
@@ -16,15 +16,16 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
