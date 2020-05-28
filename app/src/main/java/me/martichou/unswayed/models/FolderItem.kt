70d85a1507ecae882eae1b5792467acac6f9f389
Martichou <martichou.andre@gmail.com>
2020-05-28 17:10:43 +0200

    Add folder and icon if they are not backed up (placeholder for now as no backup yet)

---
+++ app/src/main/java/me/martichou/unswayed/models/FolderItem.kt
@@ -0,0 +1,5 @@
+package me.martichou.unswayed.models
+
+import android.net.Uri
+
+data class FolderItem(val folderName: String, val lastImgUri: Uri)
---
+++ app/src/main/java/me/martichou/unswayed/ui/folder/FolderAdapter.kt
@@ -0,0 +1,64 @@
+package me.martichou.unswayed.ui.folder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.text.PrecomputedTextCompat
+import androidx.core.widget.TextViewCompat
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import me.martichou.unswayed.R
+import me.martichou.unswayed.databinding.FolderRvItemBinding
+import me.martichou.unswayed.models.FolderItem
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FolderAdapter : ListAdapter<FolderItem, FolderAdapter.ViewHolder>(FolderDiff()) {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    @ExperimentalStdlibApi
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            FolderRvItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    class ViewHolder(private val binding: FolderRvItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: FolderItem) {
+            binding.apply {
+                folderName.setTextFuture(
+                    PrecomputedTextCompat.getTextFuture(
+                        item.folderName,
+                        TextViewCompat.getTextMetricsParams(binding.folderName), null
+                    )
+                )
+                Glide.with(this.folderImage)
+                    .load(item.lastImgUri)
+                    .override(binding.folderImage.width, binding.folderImage.height)
+                    .thumbnail(0.1f)
+                    .error(R.drawable.placeholder)
+                    .into(binding.folderImage)
+                executePendingBindings()
+            }
+        }
+    }
+}
+
+class FolderDiff : DiffUtil.ItemCallback<FolderItem>() {
+    override fun areItemsTheSame(oldItem: FolderItem, newItem: FolderItem): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: FolderItem, newItem: FolderItem): Boolean {
+        return oldItem.lastImgUri == newItem.lastImgUri
+    }
+}
