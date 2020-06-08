9c9b31d2d5e77d5fd32761cd05e23d5029bc59c0
Martichou <martichou.andre@gmail.com>
2020-06-08 02:22:48 +0200

    Login screen with a great design this time, will stick with it

--- app/src/main/java/me/martichou/unswayed/AuthActivity.kt
+++ app/src/main/java/me/martichou/unswayed/AuthActivity.kt
@@ -4,22 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.setupWithNavController
 import me.martichou.unswayed.databinding.AuthActivityBinding
-import me.martichou.unswayed.databinding.MainActivityBinding
 
 class AuthActivity : AppCompatActivity() {
 
-    private lateinit var navController: NavController
     private lateinit var binding: AuthActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         binding = DataBindingUtil.setContentView(this, R.layout.auth_activity)
-        navController = Navigation.findNavController(this, R.id.auth_nav)
     }
 
 }
