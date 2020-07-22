81cc76582b4e7c9cd170e2c31f8170e7cb531b67
Martichou <martichou.andre@gmail.com>
2020-07-23 01:37:08 +0200

    Add extensions for filename when we'll upload to rust server

--- app/src/main/java/me/martichou/unswayedphotos/util/Extension.kt
+++ app/src/main/java/me/martichou/unswayedphotos/util/Extension.kt
@@ -2,6 +2,11 @@ package me.martichou.unswayedphotos.util
 
 import android.content.res.Resources
 import android.util.TypedValue
+import timber.log.Timber
+import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.regex.Pattern
 
 fun String.isaValidEmail(): Boolean {
@@ -13,3 +18,27 @@ fun Float.toDp(resources: Resources): Float {
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, resources.displayMetrics)
 }
 
+data class Two(val d1: Any, val d2: Any)
+
+// "imagefile^:^date" => [imagefile, Date()]
+fun String.parseToNameAndDate(): Two? {
+    val parts = this.split("^:^")
+    if (parts.size != 2)
+        return null
+    val formatter: DateFormat = SimpleDateFormat("d-MMM-yyyy,HH:mm:ss", Locale.ENGLISH)
+    val date: Date = try {
+        formatter.parse(parts[1])!!
+    } catch (e: ParseException) {
+        Timber.e("Error when parsing string to date $e")
+        Date()
+    }
+    return Two(parts[0], date as Any)
+}
+
+// "string", Date() => "string^:^Date()"
+fun Two.toStringNameAndDate(): String? {
+    if (!(this.d1 is String && this.d2 is Date))
+        return null
+    val formatter: DateFormat = SimpleDateFormat("d-MMM-yyyy,HH:mm:ss", Locale.ENGLISH)
+    return this.d1 + "^:^" + formatter.format(this.d2)
+}
