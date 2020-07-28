2636c101b3c8e0d7bfb6900dba51044a4225b3cf
Martichou <martichou.andre@gmail.com>
2020-07-29 00:23:29 +0200

    DiffUtil moved to the adapter file cause we don't need that much separation

--- app/src/main/java/me/martichou/unswayedphotos/ui/home/adapter/HomeAdapter.kt
+++ app/src/main/java/me/martichou/unswayedphotos/ui/home/adapter/HomeAdapter.kt
@@ -2,6 +2,7 @@ package me.martichou.unswayedphotos.ui.home.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import me.martichou.unswayedphotos.data.model.recycler.HomeDate
@@ -9,7 +10,6 @@ import me.martichou.unswayedphotos.data.model.recycler.HomeGeneral
 import me.martichou.unswayedphotos.data.model.room.ImageLocal
 import me.martichou.unswayedphotos.databinding.RvHomeDateBinding
 import me.martichou.unswayedphotos.databinding.RvHomeImageBinding
-import me.martichou.unswayedphotos.ui.home.diffutil.HomeDiff
 import me.martichou.unswayedphotos.ui.home.viewholder.DateVH
 import me.martichou.unswayedphotos.ui.home.viewholder.ImageVH
 
@@ -52,4 +52,21 @@ class HomeAdapter : ListAdapter<HomeGeneral, RecyclerView.ViewHolder>(HomeDiff()
 
     override fun getItemId(position: Int): Long = position.toLong()
 
+    class HomeDiff : DiffUtil.ItemCallback<HomeGeneral>() {
+
+        override fun areItemsTheSame(oldItem: HomeGeneral, newItem: HomeGeneral): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: HomeGeneral, newItem: HomeGeneral): Boolean {
+            return if (oldItem.type != newItem.type || ((oldItem is HomeDate && newItem is ImageLocal) || (oldItem is ImageLocal && newItem is HomeDate)))
+                false
+            else if (oldItem is ImageLocal && newItem is ImageLocal)
+                oldItem.imgUri == newItem.imgUri
+            else if (oldItem is HomeDate && newItem is HomeDate)
+                oldItem.date == newItem.date
+            else
+                false
+        }
+    }
 }
